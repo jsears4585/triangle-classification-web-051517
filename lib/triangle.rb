958a465c75c63e0127d23cf7b34a376c7109027b
Jason Sears <jsears4585@gmail.com>
2017-05-20 18:36:00 -0400

    Raise all the errors

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  def initialize len_1, len_2, len_3
+    @len_1 = len_1
+    @len_2 = len_2
+    @len_3 = len_3
+  end
+
+  def kind
+    if  @len_1 == 0 && @len_2 == 0 && @len_3 == 0 ||
+        @len_1 < 0 || @len_2 < 0 || @len_3 < 0 ||
+        @len_1 + @len_2 <= @len_3 ||
+        @len_2 + @len_3 <= @len_1 ||
+        @len_3 + @len_1 <= @len_2
+          raise TriangleError
+    else
+      if @len_1 == @len_2 && @len_2 == @len_3
+        :equilateral
+      elsif @len_1 == @len_2 || @len_2 == @len_3 || @len_3 == @len_1
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    raise "Your triangle isn't valid!"
+  end
 end
